9724be0160987b970028e65daaed37d591b8c5b0
Christopher Schroeder <guitrspaz@gmail.com>
2017-05-25 15:36:37 -0400

    list of tasks [publish]

--- assets/js/testbox.js
+++ assets/js/testbox.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function() {
 	jQuery(document).on('click','.tb-toggle-btn',function(event){
 		var target=jQuery(event.currentTarget).attr('href');
-		if( jQuery(target).hasClass('in') ){
+		if( jQuery(event.currentTarget).hasClass('in') ){
 			jQuery(event.currentTarget).find('.tb-accordion-btn-text').eq(0).text('Collapse');
 		} else {
 			jQuery(event.currentTarget).find('.tb-accordion-btn-text').eq(0).text('Expand');
 		}
 	});
-	jQuery('#clearResults').toggle( jQuery('#tb-results').is(':visible') );
+	jQuery('#clearResults').toggle( (jQuery('#tb-results').is(':visible') && jQuery('#clearResults').is(':visible')) );
 	jQuery(document).on('click','.tb-file-btn',function(event){
 		event.preventDefault();
 		runTests(jQuery(event.currentTarget).attr('href'));
@@ -27,11 +27,11 @@ function clearResults(){
 	jQuery('.tb-toggle-btn').eq(0).trigger('click');
 	jQuery("#tb-results").attr('src','');
 	jQuery('#tb-results').hide();
-	jQuery('#clearResults').toggle( jQuery('#tb-results').is(':visible') );
+	jQuery('#clearResults').toggle( (jQuery('#tb-results').is(':visible') && jQuery('#clearResults').is(':visible')) );
 }
 function runTests(src){
 	jQuery('.tb-toggle-btn').eq(0).trigger('click');
 	jQuery('#tb-results').attr('src',src);
 	jQuery('#tb-results').show();
-	jQuery('#clearResults').toggle( jQuery('#tb-results').is(':visible') );
+	jQuery('#clearResults').toggle( (jQuery('#tb-results').is(':visible') && jQuery('#clearResults').is(':visible')) );
 }
